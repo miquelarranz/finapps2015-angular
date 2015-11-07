b86f348b2ca9a68e7040995362547f41d101856b
SagarLakh <djlakh@gmail.com>
2015-11-07 06:46:37 +0100

    wifi and so

--- www/js/collect.js
+++ www/js/collect.js
@@ -5,18 +5,32 @@ angular.module('starter.controllers')
       $scope.result = "";
 
       $scope.openBox = function() {
-        $http({
+        /*$http({
           method: 'POST',
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
             },
-          data: { access_token: "b1fd7e4418ce042a1779d1c3dd9fcc56f1831a64" },
+          data: { access_token: "54dd1403b4c0337bbac2182869a960b7d3acf3f5" },
           url: 'https://api.particle.io/v1/devices/54ff70066672524850431867/open'
         }).then(function successCallback(response) {
           $scope.result = "suuuuuu";
         }, function errorCallback(response) {
           $scope.result = "sa liao un monton";
-        });
+        });*/
+        data = {
+          access_token: '54dd1403b4c0337bbac2182869a960b7d3acf3f5'
+        };
+        config = {
+          headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Bearer 54dd1403b4c0337bbac2182869a960b7d3acf3f5'
+            }
+        };
+        $http.post('https://api.particle.io/v1/devices/54ff70066672524850431867/open', data,config).then(function successCallback(response) {
+          //$scope.result = "suuuuuu";
+        }, function errorCallback(response) {});
+
       }
+
 
 })
