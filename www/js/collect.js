45cd6466866d1da7e08192597d22b274edc30c5c
miquelarranz <miquelarranz@github.com>
2015-11-07 05:23:32 +0100

    A lot of things

--- www/js/collect.js
+++ www/js/collect.js
@@ -1,10 +1,19 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers')
 
-.controller('ChatsCtrl', function($scope, Chats) {
+.controller('CollectCtrl', function($scope, $http) {
 
-  $scope.chats = Chats.all();
+      $scope.result = "";
 
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+      $scope.openBox = function() {
+        $http({
+          method: 'POST',
+          data: { access_token: "b1fd7e4418ce042a1779d1c3dd9fcc56f1831a64" },
+          url: 'https://api.particle.io/v1/devices/54ff70066672524850431867/open'
+        }).then(function successCallback(response) {
+          $scope.result = "suuuuuu";
+        }, function errorCallback(response) {
+          $scope.result = "sa liao un monton";
+        });
+      }
+
 })
