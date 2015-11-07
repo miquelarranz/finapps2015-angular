f89b1edfc19f6990b5e40a7d0011a89bca715be5
miquelarranz <miquelarranz@github.com>
2015-11-07 06:28:17 +0100

    Fix

--- www/js/collect.js
+++ www/js/collect.js
@@ -7,6 +7,9 @@ angular.module('starter.controllers')
       $scope.openBox = function() {
         $http({
           method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
           data: { access_token: "b1fd7e4418ce042a1779d1c3dd9fcc56f1831a64" },
           url: 'https://api.particle.io/v1/devices/54ff70066672524850431867/open'
         }).then(function successCallback(response) {
